53e99d9952a893aa3b95ec33720e1a7902be0d71
Tania M <tania.mathern@gmail.com>
2024-01-28 10:35:49 -0800

    empty scripts

---
+++ scripts/customer_landing_to_trusted.py
@@ -0,0 +1,15 @@
+# Expected data row totals: 
+
+# Landing
+# Customer: 956
+# Accelerometer: 81273
+# Step Trainer: 28680
+
+# Trusted
+# Customer: 482
+# Accelerometer: 40981
+# Step Trainer: 14460
+
+# Curated
+# Customer: 482
+# Machine Learning: 43681
---
+++ scripts/step_trainer_trusted.py
@@ -0,0 +1,21 @@
+"""
+    Create a table `customers_curated` containing
+    accelerometer data for customers who hae agreed
+    to have their data used for research
+"""
+
+# Expected data row totals: 
+
+# Landing
+# Customer: 956
+# Accelerometer: 81273
+# Step Trainer: 28680
+
+# Trusted
+# Customer: 482
+# Accelerometer: 40981
+# Step Trainer: 14460
+
+# Curated
+# Customer: 482
+# Machine Learning: 43681
